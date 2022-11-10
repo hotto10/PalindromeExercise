d9c78ebccb9a23ebba2f9306e58b5f3e0ed17b7e
heather otto <heatherotto32@yahoo.com>
2022-11-10 10:30:22 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+       
+        public bool IsAPalindrome(string word)
+        {
+            string backwards = "";
+
+            for (int i = word.Length -1; i >=0; i--)
+            {
+                backwards += word[i];
+            }
+
+            if (word == backwards)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData ("racecar", true)]
+        [InlineData ("mom", true)]
+        [InlineData ("kayak", true)]
+        [InlineData ("suzie", false)]
 
+        public void IsAPalindromeTest(string word1, bool expected)
+        {
+            //Arrange
+            WordSmith challenger = new WordSmith();
+            //Act
+            bool actual = challenger.IsAPalindrome(word1);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
